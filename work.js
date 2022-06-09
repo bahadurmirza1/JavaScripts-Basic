03ddefc51e0412e09910a446ca56df8c1cc76a9e
bahadurmirza1 <bahadurmirza445@gmail.com>
2022-06-09 10:18:39 -0700

    JavaScriptBasic

---
+++ work.js
@@ -0,0 +1,19 @@
+let text="my name is \"mirza\"bahadur";
+let l=text.length;
+let t="abcdefghijklmnopqrstuvwxyz";
+let size=text.length;
+let s="My name is Bahadur Latif";
+let d=new Date();
+let arry=[1,21,3,4,5,6];
+//document.write(arry.pop())
+arry.push(7)
+
+document.write(Math.E)
+// document.write(arry);
+// document.write(d);
+// document.writeln();
+// document.write(t.slice(0,5));
+// document.write(s.substring(8,13))
+// Math.E
+// Math.PI
+// Math.SQRT1_2
